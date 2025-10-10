a6998086509b8aacfc9f1817e77086c01fab9abb
Hermione D. <qudaquley@gmail.com>
2025-10-10 11:31:21 +1100

    Update layout.tsx
    Added google tag manager script

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,20 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${playfair.variable} ${inter.variable}`}>
       <head>
+        {/* 🟢 GOOGLE TAG MANAGER -ADDED */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T6W22NK8');
+            `,
+          }}
+        />
          {/* 🟢 GOOGLE ANALYTICS TAG - ADDED */}
         <Script
           strategy="afterInteractive"
@@ -53,6 +67,15 @@ export default function RootLayout({
         />
       </head>
       <body className="font-sans antialiased">
+           {/* 🟢 ADDED GTM NOSCRIPT FALLBACK */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T6W22NK8"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
         <Suspense fallback={null}>
           {children}
           <SpeedInsights />
